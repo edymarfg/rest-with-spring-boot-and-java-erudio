7ce254959d85b186e15ad6b565bc2193e8f92f53
Edymar Freitas Goulart <edymar-fg10@hotmail.com>
2023-10-01 18:40:48 -0300

    first steps

---
+++ 05_FirstSteps/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/hagar/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.hagar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
